55b0ac13c4c17468cde14c7e52c068b54f87ea8d
Rexie18 <ro@students.kiron.ngo>
2017-03-24 21:16:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -47,3 +47,63 @@ function ShoppingListController($scope) {
 }
 
 })();
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// (function () {
+// 'use strict';
+
+   
+// angular.module('ShoppingListApp', [])
+// .controller('ShoppingListController', ShoppingListController)
+// .controller('ShoppingListShowController', ShoppingListShowController)
+// .service('ShoppingListService', ShoppingListService);
+
+// ShoppingListController.$inject = ['ShoppingListService'];
+// function ShoppingListAddController(ShoppingListService) {
+//   var itemAdder = this;
+
+//   itemAdder.item = shoppingList2;
+  
+//   itemAdder.addItem = function () {
+//     ShoppingListService.addItem(itemAdder.item);
+//   }
+// }
+
+
+// ShoppingListShowController.$inject = ['ShoppingListService'];
+// function ShoppingListShowController(ShoppingListService) {
+//   var showList = this;
+
+//   showList.items = ShoppingListService.getItems();
+
+//   showList.removeItem = function (itemIndex) {
+//     ShoppingListService.removeItem(itemIndex);
+//   };
+// }
+
+
+// function ShoppingListService() {
+//   var service = this;
+
+//   // List of shopping items
+//   var items = [];
+
+//   service.addItem = function (itemName, quantity) {
+//     var item = {
+//       name: itemName,
+//       quantity: quantity
+//     };
+//     items.push(item);
+//   };
+
+//   service.removeItem = function (itemIdex) {
+//     items.splice(itemIdex, 1);
+//   };
+
+//   service.getItems = function () {
+//     return items;
+//   };
+// }
+
+// })();
+
